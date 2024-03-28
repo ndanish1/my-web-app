1b1f9f8748c08d52b3c1f211f8279cb793bc7921
Naveed Danish <ndanish1@my.centennialcollege.ca>
2024-03-28 12:09:10 -0400

    Add project files.

---
+++ quiz4-demo2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace quiz4_demo2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
